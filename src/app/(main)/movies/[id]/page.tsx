a45b635bc1af5ecde59b95f3474f3a58543df4c2
Divyansh Jain <divyanshjain2022@gmail.com>
2026-04-13 02:27:34 +0530

    Fix TypeScript null check on movie.tmdb_id

--- src/app/(main)/movies/[id]/page.tsx
+++ src/app/(main)/movies/[id]/page.tsx
@@ -180,6 +180,7 @@ export default function MovieDetailPage({ params }: MovieDetailPageProps) {
     let isCancelled = false;
 
     async function refreshTmdbData() {
+      if (!movie) return;
       try {
         setIsRefreshingTmdb(true);
 
